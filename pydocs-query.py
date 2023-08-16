eb592b2d4b20fa151b497c33a54b1a64e76aa16a
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2023-08-16 10:22:36 -0400

    query

--- pydocs-query.py
+++ pydocs-query.py
@@ -42,6 +42,6 @@ qa_llm = RetrievalQA.from_chain_type(llm=llm,
                                      chain_type_kwargs={'prompt': prompt})
 
 # ask the AI chat about information in our local files
-prompt = "What version of the Python client do I need for DataStax Astra Streaming?"
+prompt = "What is DataStax Astra Streaming?"
 output = qa_llm({'query': prompt})
 print(output["result"])
