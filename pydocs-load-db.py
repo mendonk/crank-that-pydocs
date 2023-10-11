2ee9e7fa6525aa238dd11ca9e1b908eb2a927312
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2023-10-11 11:52:57 -0400

    use-numpy-for-index

--- pydocs-load-db.py
+++ pydocs-load-db.py
@@ -1,29 +1,46 @@
-"""
-This script creates a database of information gathered from text.
-"""
-
+import os
+from typing import List
 from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-import bs4
 from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+import faiss
+from faiss import IndexFlatL2
+import numpy as np
 
-# define what documents to load
+load_dotenv()
+
+# Scrape the listed website
 loader = WebBaseLoader("https://docs.langstream.ai")
-loader.requests_kwargs = {'verify':False}
-
-# interpret information in the documents
+loader.requests_kwargs = {'verify': False}
 documents = loader.load()
-splitter = RecursiveCharacterTextSplitter(chunk_size=500,
-                                          chunk_overlap=50)
+
+# Chunk text
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 texts = splitter.split_documents(documents)
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'})
 
-# create and save the local database
-db = FAISS.from_documents(texts,embeddings)
-db.save_local("faiss")
-print("Database saved to 'faiss' directory.")
+# Compute embeddings with Hugging Face
+class EmbeddingsGenerator:
+    def __init__(self, model_name: str, model_kwargs=None):
+        self.client = SentenceTransformer(model_name, **model_kwargs)
 
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        # Ensure all texts are strings
+        texts = [str(text).replace("\n", " ") for text in texts]
+        return self.client.encode(texts).tolist()
+
+embeddings_generator = EmbeddingsGenerator(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+embeddings = embeddings_generator.embed_documents(texts)
+
+# Create FAISS index for embeddings
+dimension = len(embeddings[0])  # Assuming all embeddings have the same size
+index = faiss.IndexFlatL2(dimension)
+embeddings_array = np.array(embeddings)
+index.add(embeddings_array)
+
+# Map from FAISS's integer index to actual text
+index_to_text = {i: text for i, text in enumerate(texts)}
+
+# Save the FAISS index locally
+faiss.write_index(index, "faiss_index.index")
+print("FAISS index and mapping saved.")
