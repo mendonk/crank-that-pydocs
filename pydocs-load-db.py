4e1d1c16a014eb13398c7addf7c1cd38df6b95c7
Mendon Kissling <59585235+mendonk@users.noreply.github.com>
2023-10-11 08:46:10 -0400

    use-astra

--- pydocs-load-db.py
+++ pydocs-load-db.py
@@ -1,29 +1,73 @@
-"""
-This script creates a database of information gathered from text.
-"""
-
+import os
+import uuid
+import cassio
+from typing import List
+from bs4 import BeautifulSoup
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-import bs4
 from sentence_transformers import SentenceTransformer
 
-# define what documents to load
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def getCQLSession():
+    cluster = Cluster(
+        cloud={
+            "secure_connect_bundle": os.environ['ASTRA_DB_SECURE_BUNDLE_PATH'],
+        },
+        auth_provider=PlainTextAuthProvider(
+            "token",
+            os.environ['ASTRA_DB_APPLICATION_TOKEN'],
+        ),
+    )
+    return cluster.connect()
+
+# Initialize cassIO
+cassio.init(
+    token=os.environ['ASTRA_DB_APPLICATION_TOKEN'],
+    database_id=os.environ['ASTRA_DB_ID'],
+    keyspace=os.environ['ASTRA_DB_KEYSPACE']
+)
+
+# Scrape the listed website
 loader = WebBaseLoader("https://docs.langstream.ai")
-loader.requests_kwargs = {'verify':False}
-
-# interpret information in the documents
+loader.requests_kwargs = {'verify': False}
 documents = loader.load()
-splitter = RecursiveCharacterTextSplitter(chunk_size=500,
-                                          chunk_overlap=50)
+
+# Chunk text
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 texts = splitter.split_documents(documents)
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'})
 
-# create and save the local database
-db = FAISS.from_documents(texts,embeddings)
-db.save_local("faiss")
-print("Database saved to 'faiss' directory.")
+# Compute embeddings with Hugging Face
+class EmbeddingsGenerator:
+    def __init__(self, model_name: str, model_kwargs=None):
+        self.client = SentenceTransformer(model_name, **model_kwargs)
 
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        # Ensure all texts are strings
+        texts = [str(text).replace("\n", " ") for text in texts]
+        return self.client.encode(texts).tolist()
+
+embeddings_generator = EmbeddingsGenerator(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+embeddings = embeddings_generator.embed_documents(texts)
+
+# Output to AstraDB vector database
+astraSession = getCQLSession()
+astraSession.set_keyspace(os.environ['ASTRA_DB_KEYSPACE'])
+
+for text, embedding in zip(texts, embeddings):
+    doc_id = uuid.uuid4()  # Generate a unique ID
+    actual_text_list = [text.page_content]
+    print(type(text), text)
+    print(type(embedding), embedding)
+    astraSession.execute(
+        """
+        INSERT INTO tbl2 (doc_id, list_text, embedding)
+        VALUES (%s, %s, %s)
+        """,
+        (doc_id, actual_text_list, embedding)
+    )
+    print("*", end='')
